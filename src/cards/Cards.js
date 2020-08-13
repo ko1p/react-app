57f7475381983ef5a4981bb70410f493e8c7197e
ko1p <30528165+ko1p@users.noreply.github.com>
2020-08-13 13:50:54 +0300

    add some styles

--- src/cards/Cards.js
+++ src/cards/Cards.js
@@ -3,11 +3,27 @@ import React from 'react';
 import './Cards.css';
 
 const card = (props) => {
+  const inputClasses = ['input'];
+  if (props.name !== '') {
+    inputClasses.push('green')
+  } else {
+    inputClasses.push('red')
+  }
+
+  if (props.name.length < 5) {
+    inputClasses.push('bold')
+  }
+
   return (
     <div className="Cards">
       <p>Модель: <strong>{props.name}</strong></p>
       <p>Год: <strong>{props.year}</strong></p>
-      <input type="text" onChange={props.inputHandler} value={props.name}></input>
+      <input 
+      className={inputClasses.join(' ')}
+      type="text" 
+      onChange={props.inputHandler} 
+      value={props.name}
+      ></input>
       <button onClick={props.deleteHendler}>DELETE CARD</button>
     </div>
   )

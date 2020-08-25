4fc67ac11a1476c204f174ab378b143004a7d56f
ko1p <30528165+ko1p@users.noreply.github.com>
2020-08-25 12:24:21 +0300

    learn redux step 2

--- redux.js
+++ redux.js
@@ -1,49 +1,48 @@
 const redux = require('redux')
 
 const initialState = {
-    counter: 0
+  counter: 0
 }
 
+// Reducer
 const reducer = (state = initialState, action) => {
 
-    if (action.type === 'ADD') {
-        return {
-            counter: state.counter + 1
-        }
+  if (action.type === 'ADD') {
+    return {
+      counter: state.counter + 1
     }
+  }
 
-    if (action.type === 'SUB') {
-        return {
-            counter: state.counter - 1
-        }
+  if (action.type === 'SUB') {
+    return {
+      counter: state.counter - 1
     }
+  }
 
-    if (action.type === 'ADD_NUMBER') {
-        return {
-            counter: state.counter + action.value
-        }
+  if (action.type === 'ADD_NUMBER') {
+    return {
+      counter: state.counter + action.value
     }
-    
+  }
 
-    return state
+  return state
+
 }
 
+// Store
 const store = redux.createStore(reducer)
 
 store.subscribe(() => {
-    console.log('Subscrive', store.getState())
+  console.log('Subscribe', store.getState())
 })
 
+// Actions
 const addCounter = {
-    type: 'ADD'
-}
-
-const reduceCounter = {
-    type: 'SUB'
+  type: 'ADD'
 }
 
 store.dispatch(addCounter)
 
-store.dispatch(reduceCounter)
+store.dispatch({ type: 'SUB' })
 
 store.dispatch({ type: 'ADD_NUMBER', value: 10 })
--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from './redux/rootReducer'
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer)
+
+const app = (
+  <Provider store={store}>
+    <App/>
+  </Provider>
+)
+
+ReactDOM.render(app, document.getElementById('root'));
 registerServiceWorker();

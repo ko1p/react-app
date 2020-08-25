cd3107dbe721a0c8b684c49a9d4d557c673647d0
ko1p <30528165+ko1p@users.noreply.github.com>
2020-08-25 12:59:56 +0300

    connect local state with redux

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
 import './App.scss'
 
 class App extends Component {
-  state = {
-    counter: 0
-  }
-
+  
   updateCounter(value) {
-    this.setState({
-      counter: this.state.counter + value
-    })
+    // this.setState({
+    //   counter: this.state.counter + value
+    // })
   }
 
   render() {
+    console.log('APP', this.props)
     return (
       <div className={'App'}>
-        <h1>Счетчик <strong>{this.state.counter}</strong></h1>
+        <h1>Счетчик <strong>{this.props.counter}</strong></h1>
 
         <hr/>
 
@@ -28,4 +27,10 @@ class App extends Component {
   }
 }
 
-export default App
+const mapStateToProps = (state /*, ownProps*/) => {
+  return {
+    counter: state.counter
+  }
+}
+
+export default connect(mapStateToProps)(App)

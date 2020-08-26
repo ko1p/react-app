b301e4270c0f5ef8d10d9aa858933a1990d9c2cf
ko1p <30528165+ko1p@users.noreply.github.com>
2020-08-26 11:30:15 +0300

    add loggerMiddleware

--- src/index.js
+++ src/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import rootReducer from './redux/rootReducer'
 
+// function loggerMiddleware(store) {
+//   return function(next) {
+//     return function(action) {
+//       const result = next(action)
+//       console.log('Middleware', store.getState())
+//       return result
+//     }
+//   }
+// }
 
-const store = createStore(rootReducer)
+// или
+
+const loggerMiddleware = store => next => action => {
+  console.log('Middleware', store.getState())
+  return next(action)
+}
+
+const store = createStore(rootReducer, applyMiddleware(loggerMiddleware))
 
 const app = (
   <Provider store={store}>
